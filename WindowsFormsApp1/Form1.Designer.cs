e01d5ce40d6b7bfb70f742415f05f0c645916549
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-05-22 12:13:31 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "До нового года:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "До экзамена:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(629, 398);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 78);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Завершить программу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(629, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 78);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Нажмите на меня";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 513);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private DateTime startTime;
+        private Color[] colors;
+        private int currentColorIndex;
+        private static int numberOfClicks;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 1000;
+            startTime = DateTime.Now;
+
+            timer2.Interval = 20000;
+
+            colors = new Color[]
+            {
+                Color.Black,
+                Color.Red,
+                Color.Yellow,
+                Color.Cyan,
+                Color.Blue,
+                Color.Pink,
+                Color.White
+            };
+
+            currentColorIndex = 0;
+
+            numberOfClicks = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int millisecondsElapsed = (int)(DateTime.Now - startTime).TotalMilliseconds;
+            this.Text = Convert.ToString(millisecondsElapsed);
+
+            DateTime newYear = new DateTime(DateTime.Now.Year + 1, 1, 1);
+            DateTime exam = new DateTime(2023, 6, 13);
+            TimeSpan timeLeftToNewYear = newYear - DateTime.Now;
+            TimeSpan timeLeftToExam = exam - DateTime.Now;
+            UpdateLabel1(timeLeftToNewYear);
+            UpdateLabel2(timeLeftToExam);
+
+            Color currentColor = this.BackColor;
+            Color nextColor = colors[currentColorIndex];
+            AnimateBackgroundColor(currentColor, nextColor);
+            currentColorIndex = (currentColorIndex + 1) % colors.Length;
+        }
+
+        private void UpdateLabel1(TimeSpan timeLeft)
+        {
+            label1.Text = string.Format($"До нового года: {timeLeft.Days} дней, {timeLeft.Hours}:{timeLeft.Minutes}:{timeLeft.Seconds}");
+        }
+
+        private void UpdateLabel2(TimeSpan timeLeft)
+        {
+            label2.Text = string.Format($"До экзамена: {timeLeft.Days} дней, {timeLeft.Hours}:{timeLeft.Minutes}:{timeLeft.Seconds}");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AnimateBackgroundColor(Color startColor, Color endColor)
+        {
+            int animationSteps = 50;
+            int animationDuration = 100;
+
+            int rStep = (endColor.R - startColor.R) / animationSteps;
+            int gStep = (endColor.G - startColor.G) / animationSteps;
+            int bStep = (endColor.B - startColor.B) / animationSteps;
+
+            for (int i = 0; i < animationSteps; i++)
+            {
+                int r = startColor.R + (rStep * i);
+                int g = startColor.G + (gStep * i);
+                int b = startColor.B + (bStep * i);
+
+                Color intermediateColor = Color.FromArgb(r, g, b);
+
+                this.BackColor = intermediateColor;
+
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(animationDuration / animationSteps);
+            }
+
+            this.BackColor = endColor;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if((int)(DateTime.Now - startTime).TotalMilliseconds <= 20000)
+            {
+                numberOfClicks++;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            SaveNumberToFile(numberOfClicks, "text.txt");
+            int maxNumber = ReadFromFile("text.txt").Max();
+            MessageBox.Show($"Вы нажали на кнопку {numberOfClicks} раз\nРекорд: {maxNumber}");
+        }
+
+        private static void SaveNumberToFile(int number, string fileName)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName, true))
+            {
+                writer.WriteLine(numberOfClicks);
+            }
+        }
+
+        private static List<int> ReadFromFile(string fileName)
+        {
+            List<int> numbers = new List<int>();
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    int number;
+                    try
+                    {
+                        if (int.TryParse(line, out number))
+                        {
+                            numbers.Add(number);
+                        }
+                        else
+                        {
+                            throw new Exception("Wrong format");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"{e.Message}");
+                    }
+                }
+            }
+            return numbers;
+        }
+    }
+}
